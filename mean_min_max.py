63aa7fe41a77370d52e639fdbc239c37a34be575
zach-davis <zachdavis99@gmail.com>
2018-03-07 10:08:46 -0500

    Comment script.

--- mean_min_max.py
+++ mean_min_max.py
@@ -1,8 +1,10 @@
 import numpy
 import matplotlib.pyplot
 
+# Load inflammation data.
 data = numpy.loadtxt(fname='inflammation-01.csv', delimiter=',')
 
+# Make three subplots (mean, max, and min)
 fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
 
 axes1 = fig.add_subplot(1, 3, 1)
